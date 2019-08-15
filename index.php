afef94c69e0accf6c464b25ab743493b38367cf0
Tran-Dinh-Dat <trandinhdat120@gmail.com>
2019-08-15 13:36:21 +0700

    first

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 //including the database connection file
 include_once("config.php");
-
+//
 //fetching data in descending order (lastest entry first)
 //$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
 $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
